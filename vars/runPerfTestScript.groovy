1e17555212527363ccdf361ef29f77112c46387c
Kunal Kotwani <kkotwani@amazon.com>
2022-04-28 13:32:28 -0700

    Using generated git credentials

--- vars/runPerfTestScript.groovy
+++ vars/runPerfTestScript.groovy
@@ -8,7 +8,8 @@ void call(Map args = [:]) {
         s3Download(file: "config.yml", bucket: "${ARTIFACT_BUCKET_NAME}", path: "${PERF_TEST_CONFIG_LOCATION}/config.yml", force: true)
     }
 
-    sh([
+    withCredentials([usernamePassword(credentialsId: "${GITHUB_BOT_TOKEN_NAME}", usernameVariable: 'GITHUB_USER', passwordVariable: 'GITHUB_TOKEN')]) {
+       sh([
         './test.sh',
         'perf-test',
         args.insecure ? "--stack test-single-${args.buildId}-${args.architecture}-${args.buildNumber}" :
@@ -19,7 +20,8 @@ void call(Map args = [:]) {
         isNullOrEmpty(args.workload) ? "" : "--workload ${args.workload}",
         isNullOrEmpty(args.testIterations) ? "" : "--test-iters ${args.testIterations}",
         isNullOrEmpty(args.warmupIterations) ? "" : "--warmup-iters ${args.warmupIterations}",
-    ].join(' '))
+        ].join(' '))
+    }
 }
 
 boolean isNullOrEmpty(String str) { return (str == null || str.allWhitespace) }
